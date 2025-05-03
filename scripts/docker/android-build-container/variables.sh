ece073b924bb48ff59944dc7f45298ca65672ee5
Michael Keller <github@ike.ch>
2025-05-03 21:57:20 +1200

    Update Android build tools version.
    Signed-off-by: Michael Keller <github@ike.ch>

--- scripts/docker/android-build-container/variables.sh
+++ scripts/docker/android-build-container/variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# When changing Qt version remember to update the 
+# When changing Qt version remember to update the
 # qt-installer-noninteractive file as well.
 QT_VERSION=5.15
 LATEST_QT=5.15.1
 NDK_VERSION=21.3.6528147
-ANDROID_BUILDTOOLS_REVISION=29.0.3
+ANDROID_BUILDTOOLS_REVISION=34.0.0
 ANDROID_PLATFORMS=android-29
 ANDROID_NDK=ndk/${NDK_VERSION}
