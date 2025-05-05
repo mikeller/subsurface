6917d0e4ccb820dc4ee0c5825be4e0b038b27e66
Michael Keller <github@ike.ch>
2025-05-05 12:13:21 +1200

    Update NDK to 25.
    Signed-off-by: Subsurface CI <mikeller@042.ch>

--- scripts/docker/android-build-container/variables.sh
+++ scripts/docker/android-build-container/variables.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # When changing Qt version remember to update the
 # qt-installer-noninteractive file as well.
-QT_VERSION=5.15
 LATEST_QT=5.15.1
 NDK_VERSION=23.2.8568313
 ANDROID_BUILDTOOLS_REVISION=34.0.0
